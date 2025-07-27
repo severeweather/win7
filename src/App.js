3d459c67fcd7c2632f174943c1021b493ab75e38
Vladyslav Ivanov <pedrobobrenko@gmail.com>
2025-07-27 05:27:20 +0200

    startup screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Desktop } from "./pages/Desktop";
 import { FocusProvider } from "./context/useFocus";
 import { RunningAppsProvider } from "./context/useRunningApps";
+import { useState } from "react";
+
+function StartingWindows() {
+  return (
+    <div className="starting-windows">
+      <div className="starting-windows__title">Starting Windows</div>
+      <div className="starting-windows__copyright">© Microsoft Corporation</div>
+      <video
+        src="/boot-screen.mp4"
+        className="starting-windows__boot-screen"
+        muted
+        autoPlay
+      />
+    </div>
+  );
+}
 
 function App() {
-  return (
-    <FocusProvider>
-      <RunningAppsProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Desktop />} />
-          </Routes>
-        </Router>
-      </RunningAppsProvider>
-    </FocusProvider>
-  );
+  const [booted, setBooted] = useState(false);
+  if (!booted) {
+    return <StartingWindows />;
+  } else {
+    return (
+      <FocusProvider>
+        <RunningAppsProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Desktop />} />
+            </Routes>
+          </Router>
+        </RunningAppsProvider>
+      </FocusProvider>
+    );
+  }
 }
 
 export default App;
