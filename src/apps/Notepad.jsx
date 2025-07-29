e00d79b54c314c977a690f7e99ebf16eaaff3f12
Vladyslav Ivanov <pedrobobrenko@gmail.com>
2025-07-29 17:42:28 +0200

    better notepad. now works

--- src/apps/Notepad.jsx
+++ src/apps/Notepad.jsx
@@ -1,77 +1,21 @@
-import { useEffect, useState } from "react";
 import { Window } from "../components/Window";
-import { getEntityById } from "../sysEntities";
+import { MenuBar } from "../components/MenuBar";
 import { isEmpty } from "../service";
+import { useState } from "react";
 
-function NotepadRaw({ appData }) {
+export function Notepad({ runningApp }) {
+  const [text, setText] = useState("");
   return (
-    <Window appData={appData}>
+    <Window appData={runningApp}>
       <div className="notepad">
-        <header className="menu-bar">
-          <section className="menu-buttons">
-            <button className="photo-viewer menu-button" type="button">
-              File
-            </button>
-            <button className="photo-viewer menu-button" type="button">
-              Print
-            </button>
-            <button className="photo-viewer menu-button" type="button">
-              E-mail
-            </button>
-            <button className="photo-viewer menu-button" type="button">
-              Burn
-            </button>
-            <button className="photo-viewer menu-button" type="button">
-              Open
-            </button>
-          </section>
-          <button type="button" className="help-button">
-            <img draggable={false} src="help-button.svg" />
-          </button>
-        </header>
-      </div>
-    </Window>
-  );
-}
-
-function NotepadFile({ appData }) {
-  return (
-    <Window appData={appData}>
-      <div className="notepad">
-        <header className="menu-bar">
-          <section className="menu-buttons">
-            <button className="photo-viewer menu-button" type="button">
-              File
-            </button>
-            <button className="photo-viewer menu-button" type="button">
-              Print
-            </button>
-            <button className="photo-viewer menu-button" type="button">
-              E-mail
-            </button>
-            <button className="photo-viewer menu-button" type="button">
-              Burn
-            </button>
-            <button className="photo-viewer menu-button" type="button">
-              Open
-            </button>
-          </section>
-          <button type="button" className="help-button">
-            <img draggable={false} src="help-button.svg" />
-          </button>
-        </header>
+        <MenuBar />
         <section className="notepad content">
-          <textarea value={appData.data.content} />
+          <textarea
+            value={runningApp.data?.content ?? text}
+            onChange={(e) => setText(e.target.value)}
+          />
         </section>
       </div>
     </Window>
-  );
-}
-
-export function Notepad({ runningApp }) {
-  return isEmpty(runningApp.data) ? (
-    <NotepadRaw />
-  ) : (
-    <NotepadFile appData={runningApp} />
   );
 }
