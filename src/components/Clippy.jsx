d2edfc67bc9b3d35c947668161ce956dfe0c4ed7
Vladyslav Ivanov <pedrobobrenko@gmail.com>
2025-08-02 13:31:35 +0200

    refined clippy, added to menubar and photo viewer

--- src/components/Clippy.jsx
+++ src/components/Clippy.jsx
@@ -1,22 +1,44 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useRef, useState } from "react";
 
 const ClippyContext = createContext();
 
 export function ClippyContextProvider({ children }) {
-  const [clippy, setClippy] = useState({ alive: false, message: "" });
+  const [clippy, setClippy] = useState({
+    alive: false,
+    mad: false,
+    message: "",
+  });
+  const clippyMad = useRef({ hello: null, why: null });
 
   function callClippy(message) {
     if (!message) return;
 
+    if (message === "WIP") {
+      message = "This feature may be added in future versions, or it may not.";
+    }
+
     killClippy();
     setTimeout(() => {
-      setClippy({ alive: true, message: message });
+      setClippy({ alive: true, mad: false, message: message });
     }, 50);
+
+    clippyMad.current.hello = setTimeout(() => {
+      setClippy({ alive: true, mad: true, message: "Hello?!" });
+    }, 10000);
+
+    clippyMad.current.why = setTimeout(() => {
+      setClippy({ alive: true, mad: true, message: "Why am I still here?" });
+    }, 45000);
   }
 
   function killClippy() {
-    setClippy({ alive: false, message: "" });
+    setClippy({ alive: false, mad: false, message: "" });
+    clearTimeout(clippyMad.current.hello);
+    clearTimeout(clippyMad.current.why);
+    clippyMad.current.hello = null;
+    clippyMad.current.why = null;
   }
+
   return (
     <ClippyContext.Provider value={{ clippy, callClippy, killClippy }}>
       {children}
@@ -28,21 +50,41 @@ export function useClippy() {
   return useContext(ClippyContext);
 }
 
-export function Clippy() {
+export function Clippy({ buttons = ["Ok, Clippy", "Wow, rude!"] }) {
   const { clippy, killClippy } = useClippy();
 
   if (!clippy.alive) return;
 
   return (
     <div className="clippy">
-      <div className="clippy__dialog-window">
-        <p className="clippy__message">"{clippy.message}"</p>
-        <button type="button" className="clippy__button" onClick={killClippy}>
-          Ok, Clippy
-        </button>
+      <div key={clippy.message} className="clippy__dialog-window">
+        {clippy.message ? (
+          <p className="clippy__message">"{clippy.message}"</p>
+        ) : (
+          <></>
+        )}
+        <div className="clippy__buttons-wrapper">
+          {buttons.map((button) => {
+            return (
+              <button
+                type="button"
+                className="clippy__button"
+                onClick={killClippy}
+              >
+                <div className="clippy__circle"></div>
+                <div className="clippy__button-text">{button}</div>
+              </button>
+            );
+          })}
+        </div>
       </div>
       <div className="clippy__image-wrapper">
-        <img className="clippy__image" src="/clippy.gif" alt="" />
+        <img
+          className="clippy__image"
+          src={!clippy.mad ? "clippy.gif" : "clippy-knock.gif"}
+          alt=""
+          draggable={false}
+        />
       </div>
     </div>
   );
